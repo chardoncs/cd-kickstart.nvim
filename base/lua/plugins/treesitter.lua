f8fa9fc0c46c16a12b84a83567742a9e34683cda
Charles Dong <chardon_cs@proton.me>
2024-07-22 18:47:07 +0800

    Fix Treesitter config

--- base/lua/plugins/treesitter.lua
+++ base/lua/plugins/treesitter.lua
@@ -3,14 +3,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = function()
-      require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-    run = function ()
-      local co = coroutine.create(function ()
-        vim.cmd.TSUpdate()
-      end)
-
-      coroutine.resume(co)
+      require("nvim-treesitter.install").update({ with_sync = false })()
     end,
     config = function()
       require('nvim-treesitter.configs').setup {
