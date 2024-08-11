1d7a76c5129b7a6ed0877fd395d1d45278ae8c46
Charles Dong <chardon_cs@proton.me>
2024-08-11 21:02:18 +0800

    Add argument parsing

--- setup.lua
+++ setup.lua
@@ -1,37 +1,106 @@
 #!/usr/bin/env luajit
 
--- Pseudo main entry
+local program = "cd-kickstart setup"
+local version = "0.1.0"
+
+local help_text = [[
+{{name}} v{{version}}
+
+Usage:
+
+{{program}} [OPTIONS]
+
+Options:
+
+-d, --dir DIR                       Specify directory to install
+-p, --profile PROFILE               Install config as a profile
+
+-P, --plugin-profile                Make profile a plugin profile, with other
+                                    configurations remained global
+                                    -- No effect if `--profile` is not specified
+
+-u, --use PLUGIN1[,PLUGIN2,...]     Use optional plugins. Use commas to delimit
+                                    multiple plugins
+
+-h, --help                          Print this help
+-v, --version                       Version info
+]]
+
+-- Pseudo main function
 local function main(args)
-  for _, x in ipairs(args.usage) do
-    print(x)
+
+  for _, x in ipairs(args.using) do
   end
 end
 
+-- Entry point (argument parsing block)
 if not pcall(getfenv, 4) then
   local args = {
-    usage = {},
+    using = {},
+    dir = "~/.config/nvim",
+    profile = nil,
+    plugin_profile = false,
   }
 
-  local usage_in = false
+  -- Arg parse state
+  local in_state = nil
 
-  for _, a in ipairs(arg) do
-    if usage_in then
-      for u in string.gmatch(a, "[^,]+") do
-        table.insert(args.usage, u)
-      end
-
-      usage_in = false
-      goto continue
+  for i, a in ipairs(arg) do
+    if i < 1 then
+      goto continue__arg_loop
     end
 
-    if a == "-u" or a == "--usage" then
-      usage_in = true
+    -- use process
+    if in_state == "use" then
+      for u in string.gmatch(a, "[^,]+") do
+        table.insert(args.using, u)
+      end
+
+      in_state = nil
+      goto continue__arg_loop
+    end
+
+    -- dir process
+    if in_state == "dir" then
+      args.dir = a
+
+      in_state = nil
+      goto continue__arg_loop
+    end
+
+    -- profile process
+    if in_state == "profile" then
+      args.profile = a
+
+      in_state = nil
+      goto continue__arg_loop
+    end
+
+    -- Main arg reading
+    if a == "-u" or a == "--use" then
+      in_state = "use"
+    elseif a == "-d" or a == "--dir" then
+      in_state = "dir"
+    elseif a == "-p" or a == "--profile" then
+      in_state = "profile"
+    elseif a == "-P" or a == "--plugin-profile" then
+      args.plugin_profile = true
+    elseif a == "-v" or a == "--version" then
+      print(string.format("%s v%s", program, version))
+      os.exit(0)
+    elseif a == "-h" or a == "--help" then
+      local t = string.gsub(help_text, "{{name}}", program)
+      t = string.gsub(t, "{{version}}", version)
+      t = string.gsub(t, "{{program}}", arg[0])
+
+      print(t)
+      os.exit(0)
     else
       io.stderr:write(string.format("Invalid argument: %q\n", a))
       os.exit(1)
     end
 
-    ::continue::
+    ::continue__arg_loop::
   end
 
   main(args)
