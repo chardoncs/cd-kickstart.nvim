74f02130721f3fc932ac759713c8b19923e9b6f2
Charles Dong <chardon_cs@proton.me>
2024-08-14 12:21:51 +0800

    Move buffer shifting keymaps to barbar.nvim init block

--- base/lua/plugins/editor.lua
+++ base/lua/plugins/editor.lua
@@ -34,6 +34,43 @@ return {
     },
     init = function ()
       vim.g.barbar_auto_setup = false
+
+      -- Move to previous/next buffer
+      vim.keymap.set('n', '<A-h>', vim.cmd.BufferPrevious, { desc = "Previous buffer" })
+      vim.keymap.set('n', '<A-l>', vim.cmd.BufferNext, { desc = "Next buffer" })
+      -- Reorder buffer to previous/next
+      vim.keymap.set('n', '<A-H>', vim.cmd.BufferMovePrevious, { desc = "Move current buffer forward" })
+      vim.keymap.set('n', '<A-L>', vim.cmd.BufferMoveNext, { desc = "Move current buffer backward" })
+
+      -- Goto buffer in position...
+      for i = 1, 10 do
+        vim.keymap.set(
+          'n',
+          string.format("<A-%d>", i % 10),
+          function() vim.cmd.BufferGoto(i) end,
+          { desc = string.format("Go to the #%d buffer", i), }
+        )
+      end
+      vim.keymap.set('n', '<A-->', vim.cmd.BufferLast, { desc = "Go to the last buffer" })
+      -- Pin/unpin buffer
+      vim.keymap.set('n', '<A-p>', vim.cmd.BufferPin, { desc = "[P]in buffer" })
+      -- Close buffer
+      vim.keymap.set('n', '<A-x>', vim.cmd.BufferClose, { desc = "Close buffer" })
+      -- Wipeout buffer
+      --                 :BufferWipeout
+      -- Close commands
+      --                 :BufferCloseAllButCurrent
+      --                 :BufferCloseAllButPinned
+      --                 :BufferCloseAllButCurrentOrPinned
+      --                 :BufferCloseBuffersLeft
+      --                 :BufferCloseBuffersRight
+      -- Magic buffer-picking mode
+      vim.keymap.set('n', '<C-p>', vim.cmd.BufferPick)
+      -- Sort automatically by...
+      vim.keymap.set('n', '<leader>bb', vim.cmd.BufferOrderByBufferNumber, { desc = "[B]uffer order by [B]uffer number" })
+      vim.keymap.set('n', '<leader>bd', vim.cmd.BufferOrderByDirectory, { desc = "[B]uffer order by [D]irectory" })
+      vim.keymap.set('n', '<leader>bl', vim.cmd.BufferOrderByLanguage, { desc = "[B]uffer order by [L]anguage" })
+      vim.keymap.set('n', '<leader>bw', vim.cmd.BufferOrderByWindowNumber, { desc = "[B]uffer order by [W]indow number" })
     end,
     opts = {},
   },
