e2a36bf0689f9d224971f7d1027f96a0759a9063
Charles Dong <chardon_cs@proton.me>
2024-11-18 12:32:57 +0800

    Update themes

--- base/lua/plugins/themes.lua
+++ base/lua/plugins/themes.lua
@@ -1,10 +1,28 @@
 return {
+  -- Tokyo Night 
   {
     "folke/tokyonight.nvim",
     priority = 1000,
-    opts = {},
+    opts = {
+      style = "night",
+      transparent = true,
+      terminal_colors = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "dark",
+      },
+      lualine_bold = true,
+      cache = true,
+      plugins = {
+        auto = true,
+      },
+      on_colors = function (colors)
+        colors.border_highlight = colors.blue
+        colors.BufferCurrentSign = colors.border_highlight
+      end,
+    },
     init = function ()
-      vim.cmd.colorscheme("tokyonight-night")
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 }
