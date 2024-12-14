06c4a487d549ed3f468046500d172d6426b094ae
Charles Dong <chardon_cs@proton.me>
2024-12-14 21:59:05 +0800

    Remove angle separators

--- base/lua/plugins/editor.lua
+++ base/lua/plugins/editor.lua
@@ -12,6 +12,10 @@ return {
       'nvim-tree/nvim-web-devicons'
     },
     opts = {
+      options = {
+        component_separators = { left = "", right = ""},
+        section_separators = { left = "", right = ""},
+      },
       sections = {
         lualine_a = {
           {
