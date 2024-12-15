99453c188a97a14c239f80f2e1f8899807d94616
Charles Dong <chardon_cs@proton.me>
2024-12-15 13:32:56 +0800

    Add buffer removal shorthand

--- base/lua/keymaps.lua
+++ base/lua/keymaps.lua
@@ -24,8 +24,6 @@ vim.keymap.set(
   { desc = "Toggle [S]pell Check" }
 )
 
-vim.keymap.set("n", "<leader>tt", vim.cmd.tabnew, { desc = "Tabpage: Create a new [t]abpage, shorthand of `:tabnew`" })
-
 -- LSP keymaps
 
 -- Display info popup
@@ -34,3 +32,9 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "vim.lsp: Show information 
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "vim.lsp: [R]e[n]ame hovered variable/function" })
 -- Format buffer
 vim.keymap.set("n", "F", vim.lsp.buf.format, { desc = "vim.lsp: [F]ormat buffer" })
+
+-- Tabpage and buffer control
+
+vim.keymap.set("n", "<leader>tt", vim.cmd.tabnew, { desc = "Tabpage: Create a new [t]abpage, shorthand of `:tabnew`" })
+
+vim.keymap.set("n", "<C-x>", vim.cmd.bdelete, { desc = "Buffer: remove current buffer, shorthand of `:bdelete`" })
