5b659c270ec3427a6c0cd31b497ac1d84fde8a60
Charles Dong <chardon_cs@proton.me>
2024-12-16 11:10:17 +0800

    Make telescope integration optional

--- optional/lua/plugins/harpoon.lua
+++ optional/lua/plugins/harpoon.lua
@@ -6,14 +6,13 @@ return {
     event = "VeryLazy",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
+      --"nvim-telescope/telescope.nvim",
     },
     opts = {},
     init = function ()
       local harpoon = require("harpoon")
 
       vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "[H]arpoon: " })
-      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
       vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
       vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
@@ -24,30 +23,32 @@ return {
       vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
       vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
       -- Telescope integration
-      vim.keymap.set(
-        "n",
-        "<C-e>",
-        function ()
-          local telescope_conf = require("telescope.config").values
-          local files = harpoon:list()
+      --vim.keymap.set(
+      --  "n",
+      --  "<C-e>",
+      --  function ()
+      --    local telescope_conf = require("telescope.config").values
+      --    local files = harpoon:list()
 
-          local paths = {}
-          for _, item in ipairs(files) do
-            table.insert(paths, item.value)
-          end
+      --    local paths = {}
+      --    for _, item in ipairs(files) do
+      --      table.insert(paths, item.value)
+      --    end
 
-          require("telescope.pickers").new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-                results = paths,
-            }),
-            previewer = telescope_conf.file_previewer({}),
-            sorter = telescope_conf.generic_sorter({}),
-          }):find()
-        end,
-        { desc = "[H]arpoon: Open harpoon window" }
-      )
+      --    require("telescope.pickers").new({}, {
+      --      prompt_title = "Harpoon",
+      --      finder = require("telescope.finders").new_table({
+      --          results = paths,
+      --      }),
+      --      previewer = telescope_conf.file_previewer({}),
+      --      sorter = telescope_conf.generic_sorter({}),
+      --    }):find()
+      --  end,
+      --  { desc = "[H]arpoon: Open harpoon window" }
+      --)
     end
   },
 }
