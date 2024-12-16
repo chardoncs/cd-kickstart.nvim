eef1f39f89e49cb62a7118ea93aac866c247eea1
Charles Dong <chardon_cs@proton.me>
2024-12-16 11:27:52 +0800

    Update harpoon keymaps

--- optional/lua/plugins/harpoon.lua
+++ optional/lua/plugins/harpoon.lua
@@ -1,54 +1,59 @@
 return {
-  -- Harpoon
+  -- ThePrimeagen's Harpoon
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    event = "VeryLazy",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      --"nvim-telescope/telescope.nvim",
     },
     opts = {},
     init = function ()
       local harpoon = require("harpoon")
 
-      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "[H]arpoon: " })
+      vim.keymap.set(
+        "n",
+        "<leader>hh",
+        function()
+          if pcall(function () harpoon:list():add() end) then
+            vim.print("Buffer added to Harpoon list")
+          else
+            vim.print("Error: Failed to add buffer")
+          end
+        end,
+        { desc = "[H]arpoon: Add current buffer to Harpoon list" }
+      )
 
-      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+      local ordinal_suffixes = { "st", "nd", "rd" }
+
+      for i = 1, 10 do
+        vim.keymap.set(
+          "n",
+          string.format("<C-%d>", i % 10),
+          function() harpoon:list():select(i) end,
+          { desc = string.format("Harpoon: Select the %d%s buffer in the Harpoon list", i, ordinal_suffixes[i % 10] or "th") }
+        )
+      end
 
       -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-      vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+      vim.keymap.set(
+        "n",
+        "<C-K>",
+        function() harpoon:list():prev() end,
+        { desc = "Harpoon: Switch to the previous buffer in the Harpoon list" }
+      )
+      vim.keymap.set(
+        "n",
+        "<C-J>",
+        function() harpoon:list():next() end,
+        { desc = "Harpoon: Switch to the next buffer in the Harpoon list" }
+      )
 
-      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-      -- Telescope integration
-      --vim.keymap.set(
-      --  "n",
-      --  "<C-e>",
-      --  function ()
-      --    local telescope_conf = require("telescope.config").values
-      --    local files = harpoon:list()
-
-      --    local paths = {}
-      --    for _, item in ipairs(files) do
-      --      table.insert(paths, item.value)
-      --    end
-
-      --    require("telescope.pickers").new({}, {
-      --      prompt_title = "Harpoon",
-      --      finder = require("telescope.finders").new_table({
-      --          results = paths,
-      --      }),
-      --      previewer = telescope_conf.file_previewer({}),
-      --      sorter = telescope_conf.generic_sorter({}),
-      --    }):find()
-      --  end,
-      --  { desc = "[H]arpoon: Open harpoon window" }
-      --)
+      vim.keymap.set(
+        "n",
+        "<leader>hm",
+        function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+        { desc = "[H]arpoon: Open Harpoon list [m]enu" }
+      )
     end
   },
 }
