4bd6848acf0340558d5ad53256667aa0cc0f36b7
Charles Dong <chardon_cs@proton.me>
2025-01-12 20:23:41 +0800

    LazyGit use existing buffer

--- base/lua/keymaps.lua
+++ base/lua/keymaps.lua
@@ -69,6 +69,8 @@ vim.keymap.set(
   "n", "<leader>gg",
   function ()
     -- Use existing tabpage with a buffer (hopefully) running LazyGit
+    local existing_buf = nil
+
     for _, b in pairs(vim.api.nvim_list_bufs()) do
       if vim.api.nvim_buf_get_name(b):match("/LazyGit$") then
         for i, t in ipairs(vim.api.nvim_list_tabpages()) do
@@ -81,14 +83,19 @@ vim.keymap.set(
             end
           end
         end
-        return
+        existing_buf = b
+        break
       end
     end
 
     vim.cmd.tabnew()
-    vim.cmd.terminal()
-    vim.fn.chansend(vim.bo.channel, "lazygit\n")
-    vim.cmd[[keepalt file LazyGit]]
+    if existing_buf then
+      vim.api.nvim_win_set_buf(0, existing_buf)
+    else
+      vim.cmd.terminal()
+      vim.fn.chansend(vim.bo.channel, "lazygit\n")
+      vim.cmd[[keepalt file LazyGit]]
+    end
     vim.cmd.startinsert()
   end,
   { desc = "Open Lazy[G]it in a builtin terminal in a new tabpage" }
