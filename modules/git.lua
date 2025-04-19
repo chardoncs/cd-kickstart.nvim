4877ac54e3766b1ea299c1b78906d5e7b8da28c7
Charles Dong <chardon_cs@proton.me>
2025-04-19 09:58:43 +0800

    Reintroduce Neogit into the `git` module

--- modules/git.lua
+++ modules/git.lua
@@ -1,5 +1,21 @@
 return {
   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      {
+        "<leader>gg",
+        "<cmd>Neogit<cr>",
+        desc = "Neo[g]it: Open Neogit",
+      },
+    },
+  },
+  {
     "sindrets/diffview.nvim",
     event = "VeryLazy",
   },
