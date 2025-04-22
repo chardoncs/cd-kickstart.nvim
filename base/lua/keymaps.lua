f95d899c7a6aa8b0c5a76e087b78e73b533ff39e
Charles Dong <chardon_cs@proton.me>
2025-04-22 17:44:10 +0800

    Add tab moving keymaps

--- base/lua/keymaps.lua
+++ base/lua/keymaps.lua
@@ -39,6 +39,7 @@ vim.keymap.set("n", "<M-t>", vim.cmd.tabnew, { desc = "Tabpage: Create a new [t]
 
 local ordinal_suffixes = { "st", "nd", "rd" }
 
+-- Tab switching
 for i = 1, 10 do
   vim.keymap.set(
     "n",
@@ -53,9 +54,26 @@ for i = 1, 10 do
 end
 
 vim.keymap.set("n", "<M-p>", vim.cmd.tabprevious, { desc = "Tabpage: Go to the [p]revious tabpage, shorthand of `:tabprevious`" })
-
 vim.keymap.set("n", "<M-n>", vim.cmd.tabnext, { desc = "Tabpage: Go to the [n]ext tabpage, shorthand of `:tabnext`" })
 
+-- Tab moving 
+for i = 1, 10 do
+  vim.keymap.set(
+    "n",
+    string.format("<M-S-%d>", i % 10),
+    function ()
+      if not pcall(vim.cmd.tabmove, i) then
+        vim.print(string.format("Tabpage %d does not exist", i))
+      end
+    end,
+    { desc = string.format("Tabpage: Move current tabpage to the %d%s", i, ordinal_suffixes[i % 10] or "th") }
+  )
+end
+
+vim.keymap.set("n", "<M-S-p>", function () vim.cmd.tabmove("-") end, { desc = "Tabpage: Move current tabpage leftwards, shorthand of `:tabmove -`" })
+vim.keymap.set("n", "<M-S-n>", function () vim.cmd.tabmove("+") end, { desc = "Tabpage: Move current tabpage rightwards, shorthand of `:tabmove +`" })
+
+-- Open Terminal
 vim.keymap.set(
   "n", "<leader>tt",
   function ()
