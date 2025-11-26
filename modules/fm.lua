cd0a0aed48e4b80a51c77d74e356eedfa2c486f6
Charles Dong <chardon_cs@proton.me>
2025-11-26 21:53:45 +0800

    Add common commands

--- modules/fm.lua
+++ modules/fm.lua
@@ -2,5 +2,24 @@ return {
   {
     "stevearc/oil.nvim",
     opts = {},
+    init = function ()
+      vim.api.nvim_create_user_command("Explore", "Oil", { desc = "Alias of `Oil`" })
+      vim.api.nvim_create_user_command(
+        "Sexplore",
+        function ()
+          vim.cmd.split()
+          vim.cmd.Oil()
+        end,
+        { desc = "Split the window horizontally and open file manager (oil.nvim)" }
+      )
+      vim.api.nvim_create_user_command(
+        "Vexplore",
+        function ()
+          vim.cmd.vsplit()
+          vim.cmd.Oil()
+        end,
+        { desc = "Split the window vertically and open file manager (oil.nvim)" }
+      )
+    end,
   },
 }
