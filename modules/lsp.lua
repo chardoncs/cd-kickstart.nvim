ae952f36337fe603ba2b6db8d396bb3fe649c7d0
Charles Dong <chardon_cs@proton.me>
2026-01-07 11:38:21 +0800

    Update LSP config
    Rephrase

--- modules/lsp.lua
+++ modules/lsp.lua
@@ -1,11 +1,113 @@
+--- `nvim-cmp` adds code completion onto Neovim.
+--- You can disable it to use LSP only.
+local enable_cmp = true
+
+local function register_lsp(default_config)
+  --local root_pattern = require('lspconfig').util.root_pattern
+
+  local function concat_table(first, second)
+    local output = {}
+
+    for key, value in pairs(first) do
+      output[key] = value
+    end
+
+    for key, value in pairs(second) do
+      output[key] = value
+    end
+
+    return output
+  end
+
+  local function enable_configs(entries)
+    local names = {}
+
+    for key, value in pairs(entries) do
+      local name
+      local config
+
+      -- Assign `name` and `config` based on the key-value types
+      if type(key) == "string" and type(value) == "table" then
+        name = key
+        config = concat_table(default_config, value)
+      elseif type(value) == "string" then
+        name = value
+        config = default_config
+      end
+
+      if name and config then
+        vim.lsp.config(name, config)
+        table.insert(names, name)
+      end
+    end
+
+    if #names > 0 then
+      vim.lsp.enable(names)
+    end
+  end
+
+  enable_configs({
+    -- LSP names here for configuration
+    --
+    -- Check `:help lspconfig-all` for all options.
+    --
+    -- E.g.
+    --
+    --"bashls",
+    --"clangd",
+    --"rust_analyzer",
+    --"zls",
+    --denols = {
+    --  root_markers = { "deno.json", "deno.jsonc" },
+    --},
+    --eslint = {
+    --  root_dir = root_pattern('^eslint%.config%.[mc]?[jt]s$') or vim.fn.getcwd(),
+    --},
+    --lua_ls = {
+    --  on_init = function(client)
+    --    local path = vim.fn.getcwd()
+    --    -- Use local config file if exists
+    --    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+    --      return
+    --    end
+
+    --    -- Default config
+    --    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+    --      runtime = {
+    --        version = 'LuaJIT'
+    --      },
+    --      workspace = {
+    --        checkThirdParty = false,
+    --        library = {
+    --          -- Make the server aware of Neovim runtime files
+    --          vim.env.VIMRUNTIME,
+    --        },
+    --      },
+    --    })
+    --  end,
+    --  settings = {
+    --    Lua = {},
+    --  },
+    --},
+  })
+
+  -- Other config here for LSP servers that need special care.
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
-    lazy = true,
+    lazy = enable_cmp,
+    init = function ()
+      if not enable_cmp then
+        register_lsp({})
+      end
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
     lazy = true,
+    enabled = enable_cmp,
     dependencies = {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
@@ -43,13 +145,17 @@ return {
           { name = "emoji" },
           { name = "spell" },
           { name = "otter" },
-        }
+        },
+        completion = {
+          autocomplete = true,
+        },
       }
     end,
   },
   {
     "hrsh7th/cmp-nvim-lsp",
-    event = "VeryLazy",
+    event = enable_cmp and "VeryLazy" or nil,
+    enabled = enable_cmp,
     dependencies = {
       "neovim/nvim-lspconfig",
       "hrsh7th/nvim-cmp",
@@ -57,100 +163,9 @@ return {
     },
     config = function ()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      --local root_pattern = require("lspconfig").util.root_pattern
-
-      local function concat_table(first, second)
-        local output = {}
-
-        for key, value in pairs(first) do
-          output[key] = value
-        end
-
-        for key, value in pairs(second) do
-          output[key] = value
-        end
-
-        return output
-      end
-
-      local function enable_configs(entries)
-        local default_extension = {
-          capabilities = capabilities,
-        }
-
-        local names = {}
-
-        for key, value in pairs(entries) do
-          local name
-          local config
-
-          -- Assign `name` and `config` based on the key-value types
-          if type(key) == "string" and type(value) == "table" then
-            name = key
-            config = concat_table(default_extension, value)
-          elseif type(value) == "string" then
-            name = value
-            config = default_extension
-          end
-
-          if name and config then
-            vim.lsp.config(name, config)
-            table.insert(names, name)
-          end
-        end
-
-        if #names > 0 then
-          vim.lsp.enable(names)
-        end
-      end
-
-      enable_configs({
-        -- LSP names here for configuration
-        --
-        -- Check `:help lspconfig-all` for all options.
-        --
-        -- E.g.
-        --
-        --"bashls",
-        --"clangd",
-        --"rust_analyzer",
-        --"zls",
-        --denols = {
-        --  root_markers = { "deno.json", "deno.jsonc" },
-        --},
-        --eslint = {
-        --  root_dir = root_pattern('^eslint%.config%.[mc]?[jt]s$') or vim.fn.getcwd(),
-        --},
-        --lua_ls = {
-        --  on_init = function(client)
-        --    local path = vim.fn.getcwd()
-        --    -- Use local config file if exists
-        --    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-        --      return
-        --    end
-
-        --    -- Default config
-        --    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        --      runtime = {
-        --        version = 'LuaJIT'
-        --      },
-        --      workspace = {
-        --        checkThirdParty = false,
-        --        library = {
-        --          -- Make the server aware of Neovim runtime files
-        --          vim.env.VIMRUNTIME,
-        --        },
-        --      },
-        --    })
-        --  end,
-        --  settings = {
-        --    Lua = {},
-        --  },
-        --},
+      register_lsp({
+        capabilities = capabilities,
       })
-
-      -- Other config here for LSP servers that need special care.
     end,
   },
   -- Trouble panel
