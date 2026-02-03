bba7cadaa0448faa2f307535f88401a0ca81ad41
Charles Dong <chardon_cs@proton.me>
2026-02-03 20:01:21 +0800

    Treesitter config rework

--- modules/treesitter.lua
+++ modules/treesitter.lua
@@ -1,38 +1,68 @@
+local M = {
+  installed_fts = {},
+  available_fts = {},
+}
+
+local function has_value(arr, val)
+    for _, value in ipairs(arr) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+function M:init()
+  self:update_fts()
+  self.available_fts = require("nvim-treesitter").get_available()
+end
+
+function M:update_fts()
+  self.installed_fts = require("nvim-treesitter").get_installed()
+end
+
 return {
   -- Treesitter pattens
   --
   -- NOTE: Requires a C compiler (GCC or Clang) and libstdc++ for compiling patterns
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = {
-          -- Add configs that should always get installed.
-          -- Otherwise, the plugin will not install them until
-          -- Neovim encounters corresponding file types.
-          --
-          -- Check supported languages at:
-          -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-          --
-          -- E.g.
-          -- "lua",
-          -- "python",
-          -- "rust",
-          -- "jsonc",
-        },
-        -- Use with `ensure_installed`
-        sync_install = false,
-        auto_install = true,
-        ignore_install = {
-          -- Ignored file types here
-        },
-        highlight = {
-          enable = true,
-          disable = {},
-          additional_vim_regex_highlighting = false,
-        },
-      }
+    opts = {
+      --install_dir = vim.fn.stdpath('data') .. '/site',
+    },
+    init = function ()
+      M:init()
+
+      local group = vim.api.nvim_create_augroup("treesitter-automation", { clear = false })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        pattern = "*",
+        callback = function()
+          local ft = vim.bo.filetype
+
+          -- Activate Tree-sitter when available
+          if has_value(M.available_fts, ft) then
+            -- Install filetype
+            if not has_value(M.installed_fts, ft) then
+              require("nvim-treesitter").install(ft)
+              M:update_fts()
+            end
+
+            vim.treesitter.start()
+          end
+        end,
+      })
+
+      -- Fold
+      vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo[0][0].foldmethod = 'expr'
+
+      -- Indentation (experimental)
+      --vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
     end,
   },
   -- Display current scope at the top
