65a85519c9675ed0a2bfa27b24a74a0ed4b6687f
Charles Dong <chardon_cs@proton.me>
2026-03-11 22:24:24 +0800

    Update themes config

--- modules/themes.lua
+++ modules/themes.lua
@@ -1,28 +1,63 @@
-return {
-  {
-    -- Tokyo Night
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    opts = {
-      style = "night",
-      transparent = true,
-      terminal_colors = false,
-      styles = {
-        sidebars = "transparent",
-        floats = "dark",
+-- Shared config for themes
+local theme_config = {
+  theme = "tokyonight",
+  style = "night",
+  transparent = false,
+  terminal_colors = false,
+  cached = true,
+}
+
+-- Set your themes here
+local theme_loaders = {
+  tokyonight = function (config)
+    return {
+      -- Tokyo Night
+      "folke/tokyonight.nvim",
+      priority = 1000,
+      opts = {
+        style = config.style,
+        transparent = config.transparent,
+        terminal_colors = config.terminal_colors,
+        styles = {
+          sidebars = config.transparent and "transparent" or "dark",
+          floats = "dark",
+        },
+        --lualine_bold = true,
+        cache = config.cached,
+        plugins = {
+          auto = true,
+        },
+        on_colors = function (colors)
+          colors.border_highlight = colors.blue
+          colors.BufferCurrentSign = colors.border_highlight
+        end,
       },
-      --lualine_bold = true,
-      cache = true,
-      plugins = {
-        auto = true,
-      },
-      on_colors = function (colors)
-        colors.border_highlight = colors.blue
-        colors.BufferCurrentSign = colors.border_highlight
+      init = function ()
+        vim.cmd.colorscheme("tokyonight")
       end,
-    },
-    init = function ()
-      vim.cmd.colorscheme("tokyonight")
-    end,
-  },
+    }
+  end,
+  kanagawa = function (config)
+    return {
+      "rebelot/kanagawa.nvim",
+      priority = 1000,
+      opts = {
+          compile = config.cached,
+          terminalColors = config.terminal_colors,
+          theme = config.style,
+      },
+      init = function ()
+        require(config.theme).load(config.style)
+      end,
+    }
+  end,
+}
+
+local function load_theme()
+  local loader_func = theme_loaders[theme_config.theme]
+  return loader_func and loader_func(theme_config) or {}
+end
+
+return {
+  load_theme(),
 }
