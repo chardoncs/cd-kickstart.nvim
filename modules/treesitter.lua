4b110034e73c15ef64a1371bb95a89794d150bee
Charles Dong <chardon_cs@proton.me>
2026-07-11 14:06:38 +0800

    Fix logic bug

--- modules/treesitter.lua
+++ modules/treesitter.lua
@@ -76,16 +76,16 @@ return {
 
       -- Indentation (experimental)
       --vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    end,
 
-    -- Filetype override
-    vim.filetype.add({
-      extension = {
-        tsx = "tsx",
-        jsx = "jsx",
-        cs = "c_sharp",
-      },
-    })
+      -- Filetype override
+      vim.filetype.add({
+        extension = {
+          tsx = "tsx",
+          jsx = "jsx",
+          cs = "c_sharp",
+        },
+      })
+    end,
   },
   -- Display current scope at the top
   {
